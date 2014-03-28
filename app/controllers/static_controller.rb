45bbafd069996880a5a1edc563a1c83248eb6304
Trevor Strieber <trevor@strieber.org>
2014-03-28 16:33:00 -0400

    Pass along the page that should be rendered.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,20 @@
 class StaticController < ApplicationController
+
   def index
+    puts 'index method'
+    @page ||= 'me'
+    render 'static/index'
+  end
+
+  def projects
+    puts 'projects method'
+    @page = 'projects'
+    render 'static/index'
+  end
+
+  def contact
+    puts 'contact method'
+    @page = 'contact'
+    render 'static/index'
   end
 end
