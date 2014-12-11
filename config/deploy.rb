ac51ee0a07c393f42aa4c72697cf878dd595cf63
Trevor Strieber <trevor@strieber.org>
2014-12-11 10:15:02 -0500

    Reconfiguring deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,15 @@ lock '3.1.0'
 set :application, 'strieber.org'
 set :repo_url,    'http://github.com/TrevorS/strieber.org.git'
 set :branch,      ENV['BRANCH_NAME'] || 'master'
-set :deploy_to,   '/var/webapps/strieber.org'
+set :deploy_to,   '/var/rails/strieber.org'
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
+
+set :port, 1010
 
 # rbenv
 set :rbenv_type,  :user
-set :rbenv_ruby,  '2.1.1'
+set :rbenv_ruby,  '2.1.5'
+
 
 namespace :deploy do
   desc 'Restart application'
