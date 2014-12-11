a30c3ee990d5eb3453b3c81b6a9c658b4b00c166
Trevor Strieber <trevor@strieber.org>
2014-12-11 10:23:25 -0500

    Fixing deploy_to option.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.3.4'
 set :application, 'strieber.org'
 set :repo_url,    'http://github.com/TrevorS/strieber.org.git'
 set :branch,      ENV['BRANCH_NAME'] || 'master'
-set :deploy_to,   '/var/rails/strieber.org'
+set :deploy_to,   '/var/www/rails/strieber.org'
 set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
 
 set :port, 1010
@@ -12,7 +12,6 @@ set :port, 1010
 # rbenv
 set :rbenv_type,  :user
 set :rbenv_ruby,  '2.1.5'
-
 
 namespace :deploy do
   desc 'Restart application'
